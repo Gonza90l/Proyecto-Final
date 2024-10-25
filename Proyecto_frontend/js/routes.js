898b08c1f519ca3e8e8a645b48a65dd764c6d1c5
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-10-25 00:08:55 -0300

    REFACTORIZAR: Se refactorizó el router y se serpararon las rutas a un archivo externo routes.js para mejor manejo

---
+++ Proyecto_frontend/js/routes.js
@@ -0,0 +1,16 @@
+// routes.js
+// Acá se define la configuración de las rutas de la aplicación. 
+//Cada ruta tiene un path, una vista asociada y puede ser protegida o no. 
+//Además, se puede especificar un rol requerido para acceder a la ruta.
+
+export const routeConfig = [
+    { path: '/', view: '/views/home.html' },
+    { path: '/menu', view: '/views/menu.html', protected: true, role: 'user' },
+    { path: '/about', view: '/views/about.html' },
+    { path: '/landing', view: '/views/landing.html' },
+    { path: '/404', view: '/views/404.html' },
+    { path: '/login', view: '/views/login.html' },
+    { path: '/register', view: '/views/register.html' },
+    { path: '/dashboard', view: '/views/user-dashboard.html', protected: true, role: 'user' }, // Dashboard para usuario regular
+    { path: '/admin-dashboard', view: '/views/admin-dashboard.html', protected: true, role: 'admin' }, // Dashboard para admins
+];
