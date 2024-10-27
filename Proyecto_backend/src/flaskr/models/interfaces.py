e7b31a043b7cbd111d73bcde5f855504c71f7a24
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-10-26 22:24:09 -0300

    REFACTORIZAR: se refactorizo la conexion a la base de datos, se crearon los modelos y las interfaces para mantener un contrato

--- Proyecto_backend/src/flaskr/models/interfaces.py
+++ Proyecto_backend/src/flaskr/models/interfaces.py
@@ -7,32 +7,40 @@ class IModel(ABC):
 
     @abstractmethod
     def execute_query(self, query, params=None):
+        """Ejecuta una consulta SQL y realiza un commit en la base de datos."""
         pass
 
     @abstractmethod
     def fetch_one(self, query, params=None):
+        """Recupera una única fila de la base de datos."""
         pass
 
     @abstractmethod
     def fetch_all(self, query, params=None):
+        """Recupera todas las filas de una consulta SQL."""
         pass
 
     @abstractmethod
     def insert(self, table, data):
+        """Inserta un nuevo registro en una tabla específica."""
         pass
 
     @abstractmethod
     def update(self, table, data, where_clause, where_params):
+        """Actualiza un registro existente en una tabla específica."""
         pass
 
     @abstractmethod
     def delete(self, table, where_clause, where_params):
+        """Elimina un registro en una tabla específica."""
         pass
 
     @abstractmethod
     def find_by_id(self, table, id):
+        """Encuentra un registro en una tabla usando su ID."""
         pass
 
     @abstractmethod
     def find_all(self, table):
+        """Recupera todos los registros de una tabla."""
         pass
