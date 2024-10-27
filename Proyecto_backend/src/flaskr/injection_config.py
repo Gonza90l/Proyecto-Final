6380b6f8ecb4a201f50a2a023e95c23ef8ee84d3
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-10-27 01:11:54 -0300

    REFACTORIZAR: se refactorizo para no utilizar patron repositorio, tambien se agregó clase base para DTOs que permite la autovalidacion

--- Proyecto_backend/src/flaskr/injection_config.py
+++ Proyecto_backend/src/flaskr/injection_config.py
@@ -2,17 +2,12 @@
 from injector import singleton, Binder
 from flask_mysqldb import MySQL
 from .services.users_service import UsersService
-from .repositories.user_repository import UserRepository
 from .controllers.controller import ExampleController
 from .controllers.users_controller import UsersController
 
-def configure(binder: Binder, mysql: MySQL):
-    # Vincular UserRepository a su implementación
-    user_repository = UserRepository(mysql)
-    binder.bind(UserRepository, to=user_repository, scope=singleton)
-    
+def configure(binder: Binder, mysql):
     # Vincular UserService a su implementación
-    user_service = UsersService(user_repository)
+    user_service = UsersService(mysql)
     binder.bind(UsersService, to=user_service, scope=singleton)
     
     # Vincular ExampleController a su implementación
--- Proyecto_backend/src/flaskr/services/users_service.py
+++ Proyecto_backend/src/flaskr/services/users_service.py
@@ -1,27 +1,144 @@
-from flaskr.repositories.user_repository import UserRepository
 from flaskr.dtos.dtos import RegisterDTO, UpdateUserDTO
+from werkzeug.security import generate_password_hash
+from flask import current_app as app
+import jwt
 
 
 class UsersService:
-    def __init__(self, user_repository: UserRepository):
-        self.user_repository = user_repository
+    def __init__(self, mysql):
+        self.mysql = mysql
 
     def register(self, register_dto: RegisterDTO):
-        user = self.user_repository.create_user(register_dto)
+        # validamos el DTO
+        errors = register_dto.validate()
+        if errors:
+            raise Exception(f"Validation errors: {errors}")
+
+        # intentamos convertir el DTO a un objeto de la base de datos
+        user = User()
+        try:
+            user.from_json_dto(register_dto)
+        except Exception as e:
+            raise Exception(f"Error converting DTO to database object: {e}")
+
+        #buscamos si no existe un usuario con el mismo email usando mysql
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (user.email))
+        user_exists = cur.fetchone() # traemos un solo resultado
+        cur.close()
+
+        # si el usuario ya existe, lanzamos una excepcion
+        if user_exists:
+            raise Exception("User with this email already exists")
+
+        #el password lo debemos hashear ya qwue no se debe guardar en texto plano
+        user.password = generate_password_hash(user.password)
+
+        # intentamos guardar el objeto en la base de datos
+        try:
+            # podemos usar la funcion save implementada en el modelo
+            # user.save()
+            # pero usamos mysql raw para no ser pythonico y usar el SQL directamente
+            cur = self.self.mysql.connection.cursor()
+            cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (user.name, user.email, user.password))
+            self.self.mysql.connection.commit()
+            cur.close()
+        except Exception as e:
+            raise Exception(f"Error saving user to database: {e}")
+        
         return user
 
-    def get_user_by_email(self, email: str):
-        user = self.user_repository.get_user_by_email(email)
+
+    def update_user(self, user_id, update_user_dto: UpdateUserDTO):
+        # validamos el DTO
+        errors = update_user_dto.validate()
+        if errors:
+            raise Exception(f"Validation errors: {errors}")
+
+        # intentamos convertir el DTO a un objeto de la base de datos
+        user = User()
+        try:
+            user.from_json_dto(update_user_dto)
+        except Exception as e:
+            raise Exception(f"Error converting DTO to database object: {e}")
+
+        # intentamos guardar el objeto en la base de datos
+        try:
+            # podemos usar la funcion save implementada en el modelo
+            # user.save()
+            # pero usamos mysql raw para no ser pythonico y usar el SQL directamente
+            cur = self.self.mysql.connection.cursor()
+            cur.execute("UPDATE users SET name = %s, email = %s WHERE id = %s", (user.name, user.email, user_id))
+            self.self.mysql.connection.commit()
+            cur.close()
+        except Exception as e:
+            raise Exception(f"Error saving user to database: {e}")
+        
         return user
 
-    def get_user_by_id(self, user_id: int):
-        user = self.user_repository.get_user_by_id(user_id)
+    def get_user(self, user_id):
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE id = %s", (user_id))
+        user = cur.fetchone()
+        cur.close()
+
         return user
 
-    def update_user(self, user_id: int, update_dto: UpdateUserDTO):
-        user = self.user_repository.update_user(user_id, update_dto)
+    def get_users(self):
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT * FROM users")
+        users = cur.fetchall()
+        cur.close()
+
+        return users
+
+    def delete_user(self, user_id):
+        cur = self.mysql.connection.cursor()
+        cur.execute("DELETE FROM users WHERE id = %s", (user_id))
+        self.mysql.connection.commit()
+        cur.close()
+
+        return True
+
+    def get_user_by_email(self, email):
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email))
+        user = cur.fetchone()
+        cur.close()
+
         return user
 
-    def delete_user(self, user_id: int):
-        user = self.user_repository.delete_user(user_id)
-        return user
+    def login (self, email, password):
+        #buscamos si no existe un usuario con el mismo email usando mysql
+        #debemos generar un token para el usuario
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT * FROM users WHERE email = %s", (email))
+        user = cur.fetchone()
+        cur.close()
+
+        if user and check_password_hash(user['password'], password):
+            token = jwt.encode({'id': user['id'], 'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24)}, app.config['SECRET_KEY'])
+            return token
+        else:
+            return None
+
+    def get_user_by_token(self, token):
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            user = self.get_user(data['id'])
+            return user
+        except:
+            return None
+
+    def get_user_role(self, user_id):
+        cur = self.mysql.connection.cursor()
+        cur.execute("SELECT role FROM users WHERE id = %s", (user_id))
+        role = cur.fetchone()
+        cur.close()
+
+        return role
+
+
+    
+
+    
