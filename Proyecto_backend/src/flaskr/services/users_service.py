607add97ad9ba2402036fdc943b0c61d8c90f2a2
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-10-27 14:23:06 -0300

    REFCTORIZAR: se mejoró la implementacion de verify-token, se aprovechó el cacheo del token en userservice

--- Proyecto_backend/src/flaskr/services/users_service.py
+++ Proyecto_backend/src/flaskr/services/users_service.py
@@ -1,8 +1,9 @@
 from flaskr.dtos.dtos import RegisterDTO, UpdateUserDTO
-from werkzeug.security import generate_password_hash
-from flask import current_app as app
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask import current_app as app, jsonify
 import jwt
-
+from datetime import datetime, timedelta
+from flaskr.auth import decode_and_cache_token  # Importar la función decode_and_cache_token
 
 class UsersService:
     def __init__(self, mysql):
@@ -21,17 +22,17 @@ class UsersService:
         except Exception as e:
             raise Exception(f"Error converting DTO to database object: {e}")
 
-        #buscamos si no existe un usuario con el mismo email usando mysql
+        # buscamos si no existe un usuario con el mismo email usando mysql
         cur = self.mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE email = %s", (user.email))
-        user_exists = cur.fetchone() # traemos un solo resultado
+        cur.execute("SELECT * FROM users WHERE email = %s", (user.email,))
+        user_exists = cur.fetchone()  # traemos un solo resultado
         cur.close()
 
         # si el usuario ya existe, lanzamos una excepcion
         if user_exists:
             raise Exception("User with this email already exists")
 
-        #el password lo debemos hashear ya qwue no se debe guardar en texto plano
+        # el password lo debemos hashear ya que no se debe guardar en texto plano
         user.password = generate_password_hash(user.password)
 
         # intentamos guardar el objeto en la base de datos
@@ -39,15 +40,14 @@ class UsersService:
             # podemos usar la funcion save implementada en el modelo
             # user.save()
             # pero usamos mysql raw para no ser pythonico y usar el SQL directamente
-            cur = self.self.mysql.connection.cursor()
+            cur = self.mysql.connection.cursor()
             cur.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", (user.name, user.email, user.password))
-            self.self.mysql.connection.commit()
+            self.mysql.connection.commit()
             cur.close()
         except Exception as e:
             raise Exception(f"Error saving user to database: {e}")
         
         return user
-
 
     def update_user(self, user_id, update_user_dto: UpdateUserDTO):
         # validamos el DTO
@@ -67,9 +67,9 @@ class UsersService:
             # podemos usar la funcion save implementada en el modelo
             # user.save()
             # pero usamos mysql raw para no ser pythonico y usar el SQL directamente
-            cur = self.self.mysql.connection.cursor()
+            cur = self.mysql.connection.cursor()
             cur.execute("UPDATE users SET name = %s, email = %s WHERE id = %s", (user.name, user.email, user_id))
-            self.self.mysql.connection.commit()
+            self.mysql.connection.commit()
             cur.close()
         except Exception as e:
             raise Exception(f"Error saving user to database: {e}")
@@ -78,7 +78,7 @@ class UsersService:
 
     def get_user(self, user_id):
         cur = self.mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE id = %s", (user_id))
+        cur.execute("SELECT * FROM users WHERE id = %s", (user_id,))
         user = cur.fetchone()
         cur.close()
 
@@ -94,7 +94,7 @@ class UsersService:
 
     def delete_user(self, user_id):
         cur = self.mysql.connection.cursor()
-        cur.execute("DELETE FROM users WHERE id = %s", (user_id))
+        cur.execute("DELETE FROM users WHERE id = %s", (user_id,))
         self.mysql.connection.commit()
         cur.close()
 
@@ -102,7 +102,7 @@ class UsersService:
 
     def get_user_by_email(self, email):
         cur = self.mysql.connection.cursor()
-        cur.execute("SELECT * FROM users WHERE email = %s", (email))
+        cur.execute("SELECT * FROM users WHERE email = %s", (email,))
         user = cur.fetchone()
         cur.close()
 
@@ -119,7 +119,7 @@ class UsersService:
             # Generar el token JWT incluyendo el campo 'rol'
             token = jwt.encode({
                 'id': user['id'],
-                'rol': user['rol'],  # Asegúrate de que 'rol' esté en los datos del usuario
+                'rol': user['rol'],  # Agregar el campo 'rol' al token
                 'exp': datetime.utcnow() + timedelta(hours=24)
             }, app.config['SECRET_KEY'])
             return token
@@ -127,22 +127,17 @@ class UsersService:
             return None
 
     def get_user_by_token(self, token):
-        try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            user = self.get_user(data['id'])
-            return user
-        except:
+        # Utilizar decode_and_cache_token para verificar el token
+        data, error_response, status_code = decode_and_cache_token()
+        if error_response:
             return None
+        user = self.get_user(data['id'])
+        return user
 
     def get_user_role(self, user_id):
         cur = self.mysql.connection.cursor()
-        cur.execute("SELECT role FROM users WHERE id = %s", (user_id))
+        cur.execute("SELECT role FROM users WHERE id = %s", (user_id,))
         role = cur.fetchone()
         cur.close()
 
         return role
-
-
-    
-
-    
