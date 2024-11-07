cfdd15b39deb88dc1df1dd9ba141525a75501ddd
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-11-06 22:09:19 -0300

    IMPLEMENTAR: agregar eventos de formulario de login y registro en customCode.js

--- Proyecto_frontend/js/customCode.js
+++ Proyecto_frontend/js/customCode.js
@@ -4,9 +4,12 @@ import authService from './authService.js';
 import { routerInstance } from './router.js';
 
 routerInstance.onViewLoaded = () => {
+    // ***************************************************************************************
     // Código que se ejecutará después de que el router haya cargado completamente las vistas
     // Aquí agregar el código que necesites ejecutar una vez que la vista se haya cargado
+    // ***************************************************************************************
 
+    //login agregar evento al formulario de login
     const loginForm = document.getElementById('login-form');
     if (loginForm) {
         loginForm.addEventListener('submit', (event) => {
@@ -29,7 +32,25 @@ routerInstance.onViewLoaded = () => {
         });
     }
 
+    //register agregar evento al formulario de registro
+    const registerForm = document.getElementById('register-form');
+    if (registerForm) {
+        registerForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const formData = new FormData(registerForm);
+            const name = formData.get('name');
+            const lastname = formData.get('lastname');
+            const email = formData.get('email');
+            const password = formData.get('password');
+            register(name, lastname, email, password);
+        });
+    }
+
 };
+
+//*******************************************************************************************
+// Codigo general que se ejecutará en todas las vistas de la aplicación
+// ***************************************************************************************
 
 //login, esta función se encarga de hacer el login del usuario
 async function login(email, password) {
@@ -44,6 +65,25 @@ async function login(email, password) {
         }
     } catch (error) {
         console.error('Login error:', error);
+    }
+    routerInstance.hideLoading();
+}
+
+//register, esta función se encarga de registrar un nuevo usuario
+// todos los usuarios registrados serán de tipo "user"
+// el Admin se crea durante la creación de la base de datos
+async function register(name, lastname, email, password) {
+    routerInstance.showLoading();
+    try {
+        const register = await authService.register(name, lastname, email, password);
+        if (register) {
+            window.history.pushState({}, '', '/login');
+            routerInstance.router();
+        } else {
+            alert('error al registrar');
+        }
+    } catch (error) {
+        console.error('Register error:', error);
     }
     routerInstance.hideLoading();
 }
