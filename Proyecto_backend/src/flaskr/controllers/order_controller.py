efe7feb8fc00124ca838633237176b374b946ee6
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-11-11 20:36:58 -0300

    IMPLEMENTAR: chequeo de roles y permisos al editar/ crear ordenes

--- Proyecto_backend/src/flaskr/controllers/order_controller.py
+++ Proyecto_backend/src/flaskr/controllers/order_controller.py
@@ -3,6 +3,7 @@ from flask_injector import inject
 from flaskr.services.orders_service import OrderService
 from flaskr.auth import token_required
 from flaskr.dtos.create_order_request_dto import CreateOrderRequestDTO
+from flaskr.auth import get_user_id, get_user_role
 
 from flaskr.exceptions.order_service_exceptions import OrderNotFoundException
 
@@ -16,6 +17,13 @@ class OrderController(BaseController):
     def get_orders(self):
         try:
             orders = self._order_service.get_orders()
+
+            # Filtramos las ordenes por el usuario que hizo la petición, si es admin mostramos todo
+            user_role = get_user_role()
+            if user_role == "user":
+                user_id = get_user_id()
+                orders = [order for order in orders if order.user_id == user_id]
+
             # orders es una lista así que lo recorremos convirtiendo en json_dto
             json_orders = [order.to_dict_dto() for order in orders]
             # serializamos la lista de json
@@ -26,8 +34,14 @@ class OrderController(BaseController):
     @token_required
     def get_order(self, id):
         try:
+            user_role = get_user_role()
+            if user_role == "user":
+                user_id = get_user_id()
+                order = self._order_service.get_order(id)
+                if order.user_id != user_id:
+                    return self.respond_error(message="You do not have permission to access this resource", status_code=403)
+            
             order = self._order_service.get_order(id)
-            print(">>>>>>",order)
             return self.respond_success(data=order.to_dict_dto())
         except OrderNotFoundException as e:
             return self.respond_error(message=str(e), status_code=404)
@@ -51,6 +65,15 @@ class OrderController(BaseController):
 
     @token_required
     def update_order(self, id):
+
+        #si es admin puede editar todo, si es user solo si le pertenece
+        user_role = get_user_role()
+        if user_role == "user":
+            user_id = get_user_id()
+            order = self._order_service.get_order(id)
+            if order.user_id != user_id:
+                return self.respond_error(message="You do not have permission to access this resource", status_code=403)
+
         data = self.get_json_data()
         update_order_request_dto, errors = UpdateOrderRequestDTO.from_json(data)
         if errors:
