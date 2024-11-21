64915edc5a48ebb5bd04bbfbc507928acd7cc18e
Rodrigo Santander <rodrigo_01@elalambiquesauceno.com.ar>
2024-11-21 17:42:30 -0300

    REFACTORIZAR: se simplificó la funcion para obtener el año, dejandose como instrucción simple

--- Proyecto_frontend/js/customCode.js
+++ Proyecto_frontend/js/customCode.js
@@ -100,25 +100,16 @@ routerInstance.onViewLoaded = async () => {
     // Hacer que navigate esté disponible globalmente
     window.navigate = routerInstance.navigate.bind(routerInstance);
 
-    // Función para actualizar el año en el footer
-    function updateFooterYear() {
-        // Obtener el elemento con el id 'footerYear'
-        const footerYearElement = document.getElementById('footerYear');
-        
-        // Verificar si el elemento existe
-        if (footerYearElement) {
-            // Obtener el año actual
-            const currentYear = new Date().getFullYear();
-            // Asignar el año actual al elemento
-            footerYearElement.textContent = currentYear;
-        } else {
-            console.warn("El elemento con id 'footerYear' no se encontró en el DOM.");
-        }
+    // Obtener el elemento con el id 'footerYear'
+    const footerYearElement = document.getElementById('footerYear');
+    // Verificar si el elemento existe
+    if (footerYearElement) {
+        // Obtener el año actual
+        const currentYear = new Date().getFullYear();
+        // Asignar el año actual al elemento
+        footerYearElement.textContent = currentYear;
     }
-   
     
-    // Llamar a la función para actualizar el año al cargar la página
-    updateFooterYear();
 
 }
 //*******************************************************************************************
